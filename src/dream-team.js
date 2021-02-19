012afaa9382641b70542fd6b94457236ae8a78d8
Kirill Kotov <krlktv@mail.ru>
2021-02-19 14:58:07 +0300

    feat: implement the function that returns name of a team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+	if (!Array.isArray(members) || members.length === 0 || !members.filter(el => typeof el === 'string').length > 0) {
+		return false;
+	} else {
+		return members.filter(el => typeof el === 'string').map(el => el.trim().toUpperCase()[0]).sort().join('');
+	}
 };
